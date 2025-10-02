25b864b9a18c03ce2d4d489239e74f33297f2b32
LeandroBottacinEzequiel <leobottacin2@gmail.com>
2025-10-02 14:40:10 -0300

    Stasis Elevador

---
+++ Assets/Puzzle Elements/Elevador/StasisElevatorSingle.cs
@@ -0,0 +1,72 @@
+using Player.Stasis;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StasisElevatorSingle : MonoBehaviour,IStasis
+{
+    private bool _isFreezed = false;
+    public bool IsFreezed => _isFreezed;
+
+
+    public Material matStasis;
+    public readonly string _outlineThicknessName = "_BorderThickness";
+    public MaterialPropertyBlock _mpb;
+    private Renderer _rend;
+    private float _saveVelocity;
+    private ElevatorPlatform _elevatorPlatform;
+    private void Awake()
+    {
+        _mpb = new MaterialPropertyBlock();
+        _rend = GetComponent<Renderer>();
+        _elevatorPlatform = GetComponentInParent<ElevatorPlatform>();
+    }
+
+    public void StatisEffectActivate()
+    {
+        FreezeObject();
+    }
+
+    public void StatisEffectDeactivate()
+    {
+        UnfreezeObject();
+    }
+
+    private void FreezeObject()
+    {
+        if (!_isFreezed)
+        {
+            _saveVelocity = _elevatorPlatform.elevatorSpeed;
+            _elevatorPlatform.elevatorSpeed = 0;
+            
+            _isFreezed = true;
+            //splineAnimate.Pause();
+            SetOutlineThickness(1.05f);
+            SetColorOutline(Color.green, 1f);
+        }
+    }
+
+    private void UnfreezeObject()
+    {
+        if (!_isFreezed) return;
+        _isFreezed = false;
+        _elevatorPlatform.elevatorSpeed = _saveVelocity;
+        //splineAnimate.Play();
+        SetOutlineThickness(0f);
+        Color lightGreen = new Color(0.6f, 1f, 0.6f);
+        SetColorOutline(lightGreen, 1f);
+    }
+    public void SetOutlineThickness(float thickness)
+    {
+        _rend.GetPropertyBlock(_mpb);
+        _mpb.SetFloat(_outlineThicknessName, thickness);
+        _rend.SetPropertyBlock(_mpb);
+    }
+
+    public void SetColorOutline(Color color, float alpha)
+    {
+        _rend.GetPropertyBlock(_mpb);
+        _mpb.SetColor("_Color", color);
+        _rend.SetPropertyBlock(_mpb);
+    }
+}
