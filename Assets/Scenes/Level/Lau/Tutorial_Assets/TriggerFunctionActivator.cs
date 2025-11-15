75521c88f8f24080bd24c352519c4f25e563977b
LIDO <diazovejerol@gmail.com>
2025-11-15 20:07:15 -0300

    Working bait

---
+++ Assets/Scenes/Level/Lau/Tutorial_Assets/TriggerFunctionActivator.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class TriggerEventActivator : MonoBehaviour
+{
+    [Header("Evento cuando el Player entra al trigger")]
+    public UnityEvent onEnterEvent;
+
+    [Header("Evento cuando termina el tiempo activo")]
+    public UnityEvent onFinishEvent;
+
+    [Header("Duración del efecto")]
+    public float activeTime = 2f;
+
+    [Header("Tag del Player")]
+    public string playerTag = "Player";
+
+    private bool isRunning = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isRunning && other.CompareTag(playerTag))
+        {
+            StartCoroutine(ActivationRoutine());
+        }
+    }
+
+    private IEnumerator ActivationRoutine()
+    {
+        isRunning = true;
+
+        // Evento al entrar
+        onEnterEvent?.Invoke();
+
+        // Tiempo activo
+        yield return new WaitForSeconds(activeTime);
+
+        // Evento al terminar
+        onFinishEvent?.Invoke();
+
+        isRunning = false;
+    }
+}
+
+
