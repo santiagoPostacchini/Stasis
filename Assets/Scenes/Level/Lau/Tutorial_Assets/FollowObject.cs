7593594a0d4b3560ed9cce8b76a9b4d8b00da719
LeandroBottacinEzequiel <leandro.bottacin@davinci.edu.ar>
2025-11-17 21:01:36 -0300

    A

---
+++ Assets/Scenes/Level/Lau/Tutorial_Assets/FollowObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform target;          // El objeto que cae y rota
+    public Vector3 offset = new Vector3(0f, 2f, 0f);  // Altura sobre el objeto
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        // Solo copiamos posición, nunca rotación
+        transform.position = target.position + offset;
+
+        if (!target.gameObject.activeSelf) gameObject.gameObject.SetActive(false);
+    }
+}
